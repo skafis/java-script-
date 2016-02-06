ec08dd2248b40b4e2c16c2e87ed715bbd518670a
skafis <franciskipchumba5@gmail.com>
2016-02-06 20:02:40 +0300

    conditional

---
+++ conditional.js
@@ -0,0 +1,16 @@
+<script>
+
+if (true){
+    console.log('oh yeeeahhhh!!')
+}
+ if (false){
+     console.log('wait,where am i')
+ }
+
+if (5>4){
+    console.log('makes sense...')
+}
+
+if (2===5){
+    console.log
+}
